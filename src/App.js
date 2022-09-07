d49de604b01641f82ec14d2003d8643592fcd44a
usov <dimazusov@yandex.ru>
2022-09-07 23:23:18 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Button, Container, Form} from "react-bootstrap";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            {/*<nav>*/}
+            {/*    <ul>*/}
+            {/*        <li>*/}
+            {/*            <Link to="/">Home</Link>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <Link to="/auth">About</Link>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <Link to="/register">Users</Link>*/}
+            {/*        </li>*/}
+            {/*    </ul>*/}
+            {/*</nav>*/}
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/auth" element={<Auth/>}/>
+                <Route path="/register" element={<Register/>}/>
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function Auth() {
+    return <Container className={"p-3"}>
+        <div className="border d-flex align-items-center justify-content-center p-5 rounded">
+            <div class="d-flex flex-column">
+                <div className="p-2">
+                    <h1>Авторизация</h1>
+                </div>
+                <div className="p-2">
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email адрес</Form.Label>
+                            <Form.Control type="email" placeholder="Введите email"/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Пароль</Form.Label>
+                            <Form.Control type="password" placeholder="Введите пароль"/>
+                            <Form.Text className="text-muted">
+                                Пароль должен состоять не менее чем из восьми символов
+                            </Form.Text>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="С правилами сайта ознакомлен"/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Войти
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        </div>
+    </Container>;
+}
+
+function Register() {
+    return <Container className={"p-3"}>
+        <div className="border d-flex align-items-center justify-content-center p-5 rounded">
+            <div class="d-flex flex-column">
+                <div className="p-2">
+                    <h1>Регистрация</h1>
+                </div>
+                <div className="p-2">
+                    <Form>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email адрес</Form.Label>
+                            <Form.Control type="email" placeholder="Введите email"/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Пароль</Form.Label>
+                            <Form.Control type="password" placeholder="Придумайте пароль"/>
+                            <Form.Text className="text-muted">
+                                Пароль должен состоять не менее чем из восьми символов
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="name">
+                            <Form.Label>Имя</Form.Label>
+                            <Form.Control type="text" placeholder="Ваше имя"/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="surname">
+                            <Form.Label>Фамилия</Form.Label>
+                            <Form.Control type="text" placeholder="Ваша фамилия"/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="age">
+                            <Form.Label>Возраст</Form.Label>
+                            <Form.Control type="text" placeholder="Введите возраст"/>
+                        </Form.Group>
+
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault"
+                                   id="male" />
+                                <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                    Мужской
+                                </label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault"
+                                   id="female" />
+                                <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                    Женский
+                                </label>
+                        </div>
+
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlTextarea1" className="form-label">Интересы</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+
+                        <Form.Group className="mb-3" controlId="city">
+                            <Form.Label>Город</Form.Label>
+                            <Form.Control type="text" placeholder="Ваш город"/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="С правилами сайта ознакомлен"/>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Зарегистрироваться
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        </div>
+    </Container>;
+}
